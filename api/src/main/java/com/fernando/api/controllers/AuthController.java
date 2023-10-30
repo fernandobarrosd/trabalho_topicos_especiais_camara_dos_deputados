e547a99051f72028f370eb16cfe22d0dd10a15bf
fernandobarrosd <fdebarros0910-2004@hotmail.com>
2023-10-30 09:27:01 -0300

    Edit signup endpoint adding RequestBody.

--- api/src/main/java/com/fernando/api/controllers/AuthController.java
+++ api/src/main/java/com/fernando/api/controllers/AuthController.java
@@ -1,17 +1,26 @@
 package com.fernando.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fernando.api.dtos.UsuarioRequestDTO;
+import com.fernando.api.services.interfaces.UsuarioService;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+
     @PostMapping("/signup")
-    public ResponseEntity<Void> createUser() {
+    public ResponseEntity<Void> createUser(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        usuarioService.createUser(usuarioRequestDTO);
         return ResponseEntity.noContent().build();
     }
 }
